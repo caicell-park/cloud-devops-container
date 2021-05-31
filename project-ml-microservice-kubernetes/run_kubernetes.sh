8c6f8d8b769536799e5929baf22046e71607f8bd
su.park <su.park@kakaocorp.com>
2021-05-31 18:05:53 +0900

    Run env on mini kube

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=caicellsangeon/cloud-devops:ml-microservice_20210531
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment --image=$dockerpath ml-microservice
+kubectl expose deployment ml-microservice --port=80 --name=ml-microservice-http
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-microservice-5cb7bf6946-gxz86  8000:80
