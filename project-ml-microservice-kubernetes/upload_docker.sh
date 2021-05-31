18e93bc630fac82eab7238fd20663cd30fb50744
su.park <su.park@kakaocorp.com>
2021-05-31 15:14:29 +0900

    Upload docker image as caicellsangeon/cloud-devops:ml-microservice_20210531

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,7 +5,8 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=caicellsangeon/cloud-devops:ml-microservice_20210531
+docker tag ml-microservice:latest $dockerpath
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
